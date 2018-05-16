b760686197c145ba5012ad5f9e045a6b91910463
Rebekah Astle <rastle@e1z3r4p2.42.us.org>
2018-05-16 15:03:52 -0700

    This is stressful

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -6,7 +6,7 @@
 /*   By: rastle <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/09 14:53:15 by rastle            #+#    #+#             */
-/*   Updated: 2018/05/15 14:07:32 by rastle           ###   ########.fr       */
+/*   Updated: 2018/05/16 14:55:24 by rastle           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,25 +23,31 @@ void	check_pieces()
 void	check_file()
 {
 	// check if file input is valid
+	// valid file has 4 lines and then one new line
 }
 
 int		main(int argc, char** argv)
 {
-	int fd;
-	int ret;
 	char *buf;
+	int fd;//some struct map
+	int ret;
 
-
-	buf = ft_strnew(21);	
+	ret = 0;
+	fd = 0;
+	buf = ft_strnew(21);
 	if (argc != 2)
+	{
+		write(1, "usage: ./fillit input_file\n", 27);
+		exit(0);
+	}
+	if (fd = open(argv[1], O_RDONLY) == -1)
 	{
 		write(1, "error\n", 6);
 		exit(0);
 	}
-	if ((fd = open(argv[1], O_RDONLY) < 0)
-		//return error
-		return (0);
-	while (ret = read(fd, buf, BUFF_SIZE))
-	i
+	while (ret = read(fd, buf, BUFF_SIZE) >= 0)
+	{
+		
+	}
 	close(fd);
 }
